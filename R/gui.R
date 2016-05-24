8526f7e53a27f7f015bee3d12c64e2a1c6de464e
lvzhenghua <lvzhenghua@yizhen-inc.com>
2016-05-24 23:15:32 +0800

    Mdf host port

--- R/gui.R
+++ R/gui.R
@@ -6,5 +6,5 @@ RunGUI <- function() {
       stop("Could not find example directory. Try re-installing `ahp`.", call. = FALSE)
     }
     
-    shiny::runApp(appDir, display.mode = "normal")
+    shiny::runApp(appDir, port=8080, host='0.0.0.0', display.mode = "normal")
 }
